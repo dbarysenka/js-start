fcc7b1e78c1f42fcc52ea2e62e24fa5d228f97c4
dbarysenka <denis_borisenko@mail.ru>
2022-01-04 20:14:15 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use sctrict";
+
+// alert('Hello world!');
+
+
+// const result = confirm("Are you here&");
+// console.log(result);
+
+// const answer =  prompt('are you ok&', '18', '13');
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("name", "");
+// answers[1] = prompt("family name", "");
+// answers[2] = prompt("age", "");
+
+// document.write(answers)
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user  = 'Ivan';
+
+// alert(`Shalom, ${user}`);
+
+// console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(--incr, ++decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 === 8);
+
+
+
+const isDenis = false,
+        isSa = false;
+
+console.log(isDenis || !isSa);
